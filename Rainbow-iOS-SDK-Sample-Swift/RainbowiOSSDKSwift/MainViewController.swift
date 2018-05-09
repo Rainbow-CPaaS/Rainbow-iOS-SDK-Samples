aa30334af9052a1b8d8499b5078ee23533494202
Vladimir Vyskocil <vladimir.vyskocil@al-enterprise.com>
2018-05-09 12:35:36 +0200

    Display and update the total unread conversation message count in the Swift sample app

--- Rainbow-iOS-SDK-Sample-Swift/RainbowiOSSDKSwift/MainViewController.swift
+++ Rainbow-iOS-SDK-Sample-Swift/RainbowiOSSDKSwift/MainViewController.swift
@@ -18,20 +18,33 @@ import Rainbow
 
 class MainViewController: UIViewController {
     @IBOutlet weak var contactsButton: UIButton!
+    @IBOutlet weak var unreadMessagesCountLabel: UILabel!
+    
+    var totalNbOfUnreadMessagesInAllConversations = 0
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        // notifications related to the authentification on the server
         NotificationCenter.default.addObserver(self, selector: #selector(didLogin(notification:)), name: NSNotification.Name(kLoginManagerDidLoginSucceeded), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(didLogout(notification:)), name: NSNotification.Name(kLoginManagerDidLogoutSucceeded), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(failedToAuthenticate(notification:)), name: NSNotification.Name(kLoginManagerDidFailedToAuthenticate), object: nil)
+        // notification related to contact loading
         NotificationCenter.default.addObserver(self, selector: #selector(didEndPopulatingMyNetwork(notification:)), name: NSNotification.Name(kContactsManagerServiceDidEndPopulatingMyNetwork), object: nil)
+        // notifications related to unread conversation count
+        NotificationCenter.default.addObserver(self, selector: #selector(didEndLoadingConversations(notification:)), name:NSNotification.Name(kConversationsManagerDidEndLoadingConversations), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateMessagesUnreadCount(notification:)), name:NSNotification.Name(kConversationsManagerDidUpdateMessagesUnreadCount), object: nil)
     }
     
     deinit {
+        // notifications related to the authentification on the server
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name(kLoginManagerDidLoginSucceeded), object: nil)
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name(kLoginManagerDidLogoutSucceeded), object: nil)
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name(kLoginManagerDidFailedToAuthenticate), object: nil)
+        // notification related to contact loading
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name(kContactsManagerServiceDidEndPopulatingMyNetwork), object: nil)
+        // notifications related to unread conversation count
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(kConversationsManagerDidEndLoadingConversations), object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(kConversationsManagerDidUpdateMessagesUnreadCount), object: nil)
     }
     
     override func viewDidLoad() {
@@ -39,6 +52,11 @@ class MainViewController: UIViewController {
         contactsButton.isEnabled = false
     }
  
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        unreadMessagesCountLabel.text = "\(totalNbOfUnreadMessagesInAllConversations)"
+    }
+    
     @objc func didEndPopulatingMyNetwork(notification : Notification) {
         // Enforce that this method is called on the main thread
         if !Thread.isMainThread {
@@ -49,6 +67,16 @@ class MainViewController: UIViewController {
         }
         NSLog("Did end populating my network");
         contactsButton.isEnabled = true
+    }
+    
+    @objc func didEndLoadingConversations(notification : Notification) {
+        totalNbOfUnreadMessagesInAllConversations = ServicesManager.sharedInstance().conversationsManagerService.totalNbOfUnreadMessagesInAllConversations
+        NSLog("totalNbOfUnreadMessagesInAllConversations=%ld", totalNbOfUnreadMessagesInAllConversations)
+    }
+    
+    @objc func didUpdateMessagesUnreadCount(notification : Notification) {
+        totalNbOfUnreadMessagesInAllConversations = ServicesManager.sharedInstance().conversationsManagerService.totalNbOfUnreadMessagesInAllConversations
+        NSLog("totalNbOfUnreadMessagesInAllConversations=%ld", totalNbOfUnreadMessagesInAllConversations)
     }
     
     @IBAction func logoutAction(_ sender: Any) {
