8e4b4cedf7a7d9185d84b4f6e9566bf87a226c84
Vladimir Vyskocil <vladimir.vyskocil@al-enterprise.com>
2019-05-10 18:40:45 +0200

    Fix file head commentaries

--- Rainbow-iOS-SDK-Sample-Channels/RainbowiOSSDKChannels/ChannelInfoViewController.m
+++ Rainbow-iOS-SDK-Sample-Channels/RainbowiOSSDKChannels/ChannelInfoViewController.m
@@ -1,10 +1,17 @@
-//
-//  ChannelInfoViewController.m
-//  RainbowiOSSDKChannels
-//
-//  Created by Vladimir Vyskocil on 09/05/2019.
-//  Copyright © 2019 ALE. All rights reserved.
-//
+/*
+ * Rainbow SDK sample
+ *
+ * Copyright (c) 2018, ALE International
+ * All rights reserved.
+ *
+ * ALE International Proprietary Information
+ *
+ * Contains proprietary/trade secret information which is the property of
+ * ALE International and must not be made available to, or copied or used by
+ * anyone outside ALE International without its written authorization
+ *
+ * Not to be disclosed or used except in accordance with applicable agreements.
+ */
 
 #import "ChannelInfoViewController.h"
 
