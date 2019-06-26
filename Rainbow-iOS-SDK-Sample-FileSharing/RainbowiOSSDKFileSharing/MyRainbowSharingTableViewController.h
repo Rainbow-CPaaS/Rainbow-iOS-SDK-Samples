a6cd6815ad8beba69907941b726381331178b2b5
Vladimir Vyskocil <vladimir.vyskocil@al-enterprise.com>
2019-06-26 17:47:39 +0200

    Add tabs for My Rainbow sharing and received files

---
+++ Rainbow-iOS-SDK-Sample-FileSharing/RainbowiOSSDKFileSharing/MyRainbowSharingTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  MyRainbowSharingTableViewController.h
+//  RainbowiOSSDKFileSharing
+//
+//  Created by Vladimir Vyskocil on 26/06/2019.
+//  Copyright © 2019 ALE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyRainbowSharingTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Rainbow-iOS-SDK-Sample-FileSharing/RainbowiOSSDKFileSharing/ReceivedFilesTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ReceivedFilesTableViewController.h
+//  RainbowiOSSDKFileSharing
+//
+//  Created by Vladimir Vyskocil on 26/06/2019.
+//  Copyright © 2019 ALE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ReceivedFilesTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
